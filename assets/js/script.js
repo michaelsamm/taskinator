de1b9ec4e84ad17757f4873855c95e9008d8b429
msamm <msamm93@gmail.com>
2022-03-16 23:42:16 -0500

    added type to form submision

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,24 @@ var tasksToDoE1 = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
     
     event.preventDefault();
-
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    // create list item
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    var taskInfoE1 = document.createElement("div");
+    taskInfoE1.className = "task-info";
+
+    // add html content to the div
+    taskInfoE1.innerHTML = "<h3 class = 'task-name'>" + taskNameInput +"</h3><span class ='task-type'>" + taskTypeInput + "</span>";
+    
+    // add div to li
+    listItemE1.appendChild(taskInfoE1);
+    
+    // add entire list item to list
     tasksToDoE1.appendChild(listItemE1);
 };
 
